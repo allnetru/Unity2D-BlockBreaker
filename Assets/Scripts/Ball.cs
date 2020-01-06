c0960abb74c433cd4ed847fac536f20fbd9f70eb
Alexey Torochkov <all.net.ru@gmail.com>
2020-01-06 22:28:51 +0300

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] Vector2 ballVelocity;
+    [SerializeField] AudioClip[] ballSounds;
+
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    Rigidbody2D rigidbody2D;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle.transform.position;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            rigidbody2D.velocity = ballVelocity;
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = paddle.transform.position;
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted) {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+
+    Level level;
+    GameStatus gameStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+
+        gameStatus = FindObjectOfType<GameStatus>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        
+        level.BlockDestoyed();
+        gameStatus.AddToScore();
+
+        Destroy(gameObject);
+    }
+}
